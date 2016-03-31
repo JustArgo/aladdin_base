fd0646bc3847e2f10ae06260b92d804274a7a8b1
JustArgo <565148812@qq.com>
2016-03-31 10:30:26 +0800

    修改字段

---
+++ src/main/java/com/maiquan/aladdin_order/domain/MoneyReturn.java
@@ -0,0 +1,182 @@
+package com.maiquan.aladdin_order.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class MoneyReturn implements Serializable{
+
+	private static final long serialVersionUID = 2053402694735819929L;
+
+	private Integer ID;
+
+    private String returnCode;
+
+    private String mqID;
+
+    private Integer orderID;
+
+    private String orderCode;
+
+    private Integer uid;
+
+    private String status;
+
+    private String auditStatus;
+
+    private String returnReason;
+
+    private String returnDesc;
+
+    private Long applySum;
+
+    private Long realReturnSum;
+
+    private Long thirdReturnNo;
+
+    private Date returnTime;
+
+    private Date auditTime;
+
+    private Date createTime;
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getReturnCode() {
+        return returnCode;
+    }
+
+    public void setReturnCode(String returnCode) {
+        this.returnCode = returnCode == null ? null : returnCode.trim();
+    }
+
+    public String getMqID() {
+        return mqID;
+    }
+
+    public void setMqID(String mqID) {
+        this.mqID = mqID == null ? null : mqID.trim();
+    }
+
+    public Integer getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(Integer orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode == null ? null : orderCode.trim();
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getAuditStatus() {
+        return auditStatus;
+    }
+
+    public void setAuditStatus(String auditStatus) {
+        this.auditStatus = auditStatus == null ? null : auditStatus.trim();
+    }
+
+    public String getReturnReason() {
+		return returnReason;
+	}
+
+	public void setReturnReason(String returnReason) {
+		this.returnReason = returnReason;
+	}
+
+	public String getReturnDesc() {
+        return returnDesc;
+    }
+
+    public void setReturnDesc(String returnDesc) {
+        this.returnDesc = returnDesc == null ? null : returnDesc.trim();
+    }
+
+    public Long getApplySum() {
+        return applySum;
+    }
+
+    public void setApplySum(Long applySum) {
+        this.applySum = applySum;
+    }
+
+    public Long getRealReturnSum() {
+        return realReturnSum;
+    }
+
+    public void setRealReturnSum(Long realReturnSum) {
+        this.realReturnSum = realReturnSum;
+    }
+
+    public Long getThirdReturnNo() {
+        return thirdReturnNo;
+    }
+
+    public void setThirdReturnNo(Long thirdReturnNo) {
+        this.thirdReturnNo = thirdReturnNo;
+    }
+
+    public Date getReturnTime() {
+        return returnTime;
+    }
+
+    public void setReturnTime(Date returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public Date getAuditTime() {
+        return auditTime;
+    }
+
+    public void setAuditTime(Date auditTime) {
+        this.auditTime = auditTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+	@Override
+	public String toString() {
+		return "MoneyReturn [ID=" + ID + ", returnCode=" + returnCode
+				+ ", mqID=" + mqID + ", orderID=" + orderID + ", orderCode="
+				+ orderCode + ", uid=" + uid + ", status=" + status
+				+ ", auditStatus=" + auditStatus + ", returnReason="
+				+ returnReason + ", returnDesc=" + returnDesc + ", applySum="
+				+ applySum + ", realReturnSum=" + realReturnSum
+				+ ", thirdReturnNo=" + thirdReturnNo + ", returnTime="
+				+ returnTime + ", auditTime=" + auditTime + ", createTime="
+				+ createTime + "]";
+	}
+}
